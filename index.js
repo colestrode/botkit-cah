0182d78711c85569b6b5492d8ae9499fcd1bc6e2
Cole Furfaro-Strode <colestrode@gmail.com>
2015-12-27 21:16:50 -0500

    trying more logging

--- index.js
+++ index.js
@@ -6,8 +6,14 @@ var controller = Botkit.slackbot({
 
 // connect the bot to a stream of messages
 controller.spawn({
-    token: process.env.SLACK_API_TOKEN
-}).startRTM();
+  token: process.env.SLACK_API_TOKEN
+}).startRTM(function(err) {
+  if (err) {
+    throw new Error(err);
+  }
+});
+
+
 
 // give the bot something to listen for.
 controller.hears('hello','direct_message,direct_mention,mention',function(bot,message) {
