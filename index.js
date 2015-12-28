557f64e1eb2c1534c10617e14d7e79db269c2118
Cole Furfaro-Strode <colestrode@gmail.com>
2015-12-27 21:34:56 -0500

    logging more events

--- index.js
+++ index.js
@@ -13,10 +13,19 @@ controller.spawn({
   }
 });
 
+controller.on('rtm_open', function() {
+  console.log('rtm opened');
+  console.log(arguments);
+});
+
+controller.on('rtm_closed', function() {
+  console.log('rtm closed');
+  console.log(arguments);
+});
 
 
 // give the bot something to listen for.
-controller.hears('hello','direct_message,direct_mention,mention',function(bot,message) {
+controller.hears('hello','direct_message,direct_mention,mention,ambient',function(bot,message) {
 
   bot.reply(message,'Hello yourself.');
 
